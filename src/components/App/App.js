9ea329026f9cde75b3aded157d96ad361e50a806
Kimberly Dijkmans <kim.14420@outlook.com>
2023-04-09 14:24:27 +0200

    implemented App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchResults: [
+                {
+                    id: 1,
+                    name: 'Hello',
+                    artist: 'Adele',
+                    album: 'Idk'
+                },
+                {
+                    id: 2,
+                    name: 'Coffee',
+                    artist: 'In need',
+                    album: 'Mornings'
+                },
+                {
+                    id: 3,
+                    name: 'Test',
+                    artist: 'Yess',
+                    album: 'Okay'
+                },
+            ],
+            playlistName: "My playlist",
+            playlistTracks: [
+                {
+                    id: 3,
+                    name: 'Test',
+                    artist: 'Yess',
+                    album: 'Okay'
+                }
+            ]
+        }
+
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+    }
+
+    addTrack(track) {
+        if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+            return;
+        }
+        const newPlaylistTracksArray = this.state.playlistTracks;
+        newPlaylistTracksArray.push(track);
+        this.setState({
+            playlistTracks: newPlaylistTracksArray
+        })
+    }
+
+    removeTrack(track) {
+        const newPlaylistTracksArray = this.state.playlistTracks.filter(savedTrack => savedTrack.id !== track.id);
+        this.setState({
+            playlistTracks: newPlaylistTracksArray
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Ja<span className="highlight">mmm</span>ing</h1>
+                <div className="App">
+                    <SearchBar />
+                    <div className="App-playlist">
+                        <SearchResults 
+                            searchResults={this.state.searchResults} 
+                            onAdd={this.addTrack} 
+                        />
+                        <Playlist 
+                            playlistName={this.state.playlistName} 
+                            playlistTracks={this.state.playlistTracks} 
+                            onRemove={this.removeTrack} 
+                        />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
