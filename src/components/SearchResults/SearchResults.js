794847116947f5d91b2e3d4d15a23d5b0ae98908
Kimberly Dijkmans <kim.14420@outlook.com>
2023-04-09 16:23:27 +0200

    remove console log

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,7 +3,6 @@ import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component {
     render() {
-        console.log(JSON.stringify(this.props.searchResults));
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
